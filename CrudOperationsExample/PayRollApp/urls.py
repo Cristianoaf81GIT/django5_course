f1b12c16160b456a18106fd0e850d9668a10de2a
Cristianoaf81GIT <cristianoaf81@gmail.com>
2024-05-18 11:28:39 -0300

    feat(PayRollApp): displaying employee details
    Closes #main

--- CrudOperationsExample/PayRollApp/urls.py
+++ CrudOperationsExample/PayRollApp/urls.py
@@ -3,4 +3,11 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("EmployeeList", views.employee_list, name="EmployeeList")]
+urlpatterns = [
+    path("EmployeeList", views.employee_list, name="EmployeeList"),
+    path(
+        "EmployeeDetails/<int:id>",
+        views.employee_details,
+        name="EmployeeDetails",  # noqa
+    ),  # noqa
+]
--- CrudOperationsExample/PayRollApp/views.py
+++ CrudOperationsExample/PayRollApp/views.py
@@ -11,3 +11,10 @@ def employee_list(request: HttpRequest) -> HttpResponse:
     template_file = "PayRollApp/EmployeesList.html"
     context: Dict[str, list[Employee]] = dict({"Employees": employees})
     return render(request=request, template_name=template_file, context=context)  # noqa
+
+
+def employee_details(request: HttpRequest, id: int) -> HttpResponse:
+    template_file = "PayRollApp/EmployeeDetails.html"
+    employee: Employee = Employee.objects.get(id=id)  # pyright: ignore
+    context: Dict[str, Employee] = {"Employee": employee}
+    return render(request=request, template_name=template_file, context=context)  # noqa
