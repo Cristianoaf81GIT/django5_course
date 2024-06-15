48799afc69ab17fcb2e49d443036ce2578eb34d6
cristianoaf81 <cristianoaf81@gmail.com>
2024-06-15 11:22:16 -0300

    feat(PayRollApp): sedding database tables with fixtures
    Closes #main

--- CrudOperationsExample/PayRollApp/models.py
+++ CrudOperationsExample/PayRollApp/models.py
@@ -1,11 +1,24 @@
-from typing import Tuple, List
+from typing import Any, Tuple, List
 from django.db import models
 
+
 # Create your models here.
+class Department(models.Model):
+    dept_name = models.CharField(max_length=30)
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self) -> Any:
+        return self.dept_name
+
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=30)
+
+    def __str__(self) -> Any:
+        return self.country_name
 
 
 class Employee(models.Model):
-
     countries: List[Tuple[str, str]] = [
         ("IND", "India"),
         ("BR", "Brazil"),
