1b1632161fca2f342e34f7f2f749012a93bc8a91
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-09-14 16:52:20 -0300

    feat(84): styling error message
    Closes#main

--- validationdemoproject/validationsdemoapp/models.py
+++ validationdemoproject/validationsdemoapp/models.py
@@ -17,13 +17,13 @@ def validate_favwebsite(url: str) -> None:
 # Create your models here.
 class UserRegistration(models.Model):
     username = models.CharField(max_length=15,verbose_name="User Name",validators=[MinLengthValidator(5, message="nome tem q ter pelo menos 5 caracteres")])
-    password = models.CharField(max_length=15,verbose_name="Password",validators=[MinLengthValidator(5)])
-    confirm_password = models.CharField(max_length=15,verbose_name="Confirm Password",validators=[MinLengthValidator(5)])
+    password = models.CharField(max_length=15,verbose_name="Password",validators=[MinLengthValidator(5, message="a senha tem q ter pelo menos 5 caracteres")])
+    confirm_password = models.CharField(max_length=15,verbose_name="Confirm Password",validators=[MinLengthValidator(5, message="a contra senha tem q ter no mínimo 5 caracteres e coincidier com a senha")])
     gender = models.CharField(max_length=10,verbose_name="Gender")
     country = models.CharField(max_length=20,verbose_name="Country")
     date_of_birth = models.DateField(verbose_name="Date of Birth")
     email = models.EmailField(verbose_name="E-mail")
-    postal_code = models.IntegerField(verbose_name="Postal Code",validators=[MinValueValidator(1000), MaxValueValidator(9999999)])
+    postal_code = models.IntegerField(verbose_name="Postal Code",validators=[MinValueValidator(1000, message="codigo postal deve ser maior ou igual 1000"), MaxValueValidator(9999999, message="codigo postal deve ter no máxio 9999999")])
     phone_number = models.CharField(max_length=13,verbose_name="Phone Number")
     profile = models.TextField(verbose_name="Profile of User", blank=True)
     website_url = models.URLField(verbose_name="Website Url")
